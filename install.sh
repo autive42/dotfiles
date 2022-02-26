52aa147ceca9d4f40e239781e7e32dd36f1d7957
autive42 <eheiner1764@gmail.com>
2022-02-25 20:33:14 -0700

    added inputrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # bash
 ln -s ${BASEDIR}/bash_aliases ~/.bash_aliases
+ln -s ${BASEDIR}/inputrc ~/.inputrc
 
 # vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
